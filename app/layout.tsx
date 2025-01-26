1594890747d0e05323363e3edc0debdfc628bada
VrajBirje <vrajbirje0309@gmail.com>
2025-01-26 21:53:29 +0530

    sign in change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ClerkProvider } from "@clerk/nextjs";
-import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
+// import { ClerkProvider } from "@clerk/nextjs";
+// import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,19 +25,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    // <ClerkProvider>
       <html lang="en">
         <body
           className={`${geistSans.variable} ${geistMono.variable} antialiased`}
         >
-          <SignedOut>
+          {/* <SignedOut>
             <SignIn routing="hash"  />
           </SignedOut>
-          <SignedIn>
+          <SignedIn> */}
             {children}
-          </SignedIn>
+          {/* </SignedIn> */}
         </body>
       </html>
-    </ClerkProvider>
+    // </ClerkProvider>
   );
 }
